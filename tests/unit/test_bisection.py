ce85d764c9fdd78fbc053bf0653cd73ddfa21b03
Devon Vorster <devo@telus.net>
2026-01-13 21:48:04 -0800

    Add tests for input handling

--- tests/unit/test_bisection.py
+++ tests/unit/test_bisection.py
@@ -54,7 +54,11 @@ def test_bisection_output():
 
 def test_bisection_bad_input():
     # check correct input types - typeErrors
-    pass
+    with pytest.raises(TypeError):
+        bisection(func, xmin="2", xmax=1, tol=1e-9, max_iter=200)
+
+    with pytest.raises(TypeError):
+        bisection(func, xmin=2, xmax="1", tol=1e-9, max_iter=200)
 
 
 def test_bisection_runtime_error():
